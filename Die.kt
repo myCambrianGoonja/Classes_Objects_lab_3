b6f4b95ef4f064c041703e36d7c0018b5dea8e87
myCambrianGoonja <A00277328@mycambrian.ca>
2023-06-15 21:47:44 -0400

    With two parameters condition added

--- Die.kt
+++ Die.kt
@@ -2,11 +2,11 @@ package game
 
 import kotlin.random.Random
 
-enum class Color(val otherName: String) {
-    RED("d3"),
-    WHITE("d4"),
-    YELLOW("d6"),
-    ORANGE("d20")
+enum class Color(val otherName: String, val hexCode: String) {
+    RED("d3", "#FF0000"),
+    WHITE("d4", "#FFFFFF"),
+    YELLOW("d6", "#FFFF00"),
+    ORANGE("d20", "#FFA500")
 }
 
 enum class Sides(val value: Int) {
@@ -27,7 +27,8 @@ enum class Sides(val value: Int) {
 
  */
 class Die {
-     var color: Color? = null
+    //Setting up all the values to be null in start
+    var color: Color? = null
     var numberOfSides: Sides? = null
     var sideUp: Int? = null
 
@@ -48,6 +49,21 @@ class Die {
         }
     }
 
+    constructor(numOfSides:Int, selectedColor: String) {
+        val validNumberOfSides = Sides.values().find { it.value == numOfSides }
+        val validColor = Color.values().find { it.otherName == selectedColor } ?: Color.values().find { it.hexCode == selectedColor }
+        if(validColor != null && validNumberOfSides != null) {
+            numberOfSides = validNumberOfSides
+            color = validColor
+            roll()
+        } else {
+            println("Please enter valid values, values allowed for Colors are")
+            val allColors = Color.values()
+            for (color in allColors) {
+                println("Color: ${color.name}, Other Name: ${color.otherName}, Hex Code: ${color.hexCode}")
+            }
+        }
+    }
     fun roll() {
         println("Rolling the dice")
         sideUp = Random.nextInt(1, numberOfSides?.value?.plus(1)?: 1)
