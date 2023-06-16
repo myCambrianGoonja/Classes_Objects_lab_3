50a245cea437e77a7a567e63f860eb81a9c856cd
myCambrianGoonja <A00277328@mycambrian.ca>
2023-06-15 22:50:47 -0400

    Added test cases for all the tree conditions

--- DiceGame.kt
+++ DiceGame.kt
@@ -1,28 +1,57 @@
 package game
 
+
+enum class NumOfParamers(val meaning:String){
+    ZEROP("Zero parameters"),
+    ONEP("One Parameter")
+}
+
 fun main() {
     testCases()
 }
 
 fun testCases() {
+
+
     val die = Die() //numberOfSide = SIX && color = YELLOW && sideUp = any number between 1 to 6
     val die1 = Die(3) //numberOfSide = THREE && color = RED && sideUp = any number between 1 to 3
     val die2 = Die(4) //numberOfSide = FOUR && color = WHITE && sideUp = any number between 1 to 4
     val die3 = Die(5) //ERROR because the number of sides with 5 is not an option
     val die4 = Die(6) // Same as default || die results
     val die5 = Die(20) //numberOfSide = TWENTY && color = ORANGE && sideUp = any number between 1 to 20
+    
+    val die6 = Die(20, "#FFFFFF") //numberOfSides = TWENTY && color = WHITE && sideUp = any number between 1 to 20
+    val die7 = Die(3, "RED") //numberOfSides = THREE && color = RED && sideUp = any number between 1 to 3
+    val die8 = Die(4, "GREEN") // Error invlid entry
 
-    printResults(die, null)
-    printResults(die1, 3)
-    printResults(die2, 4)
-    printResults(die3, 5)
-    printResults(die4, 6)
-    printResults(die5, 20)
+    printResults(die, null, NumOfParamers.ZEROP.name) // ZERO PARAMETER
+   
+    printResults(die1, 3, NumOfParamers.ONEP.name) //ONE PARAMETER
+    printResults(die2, 4, NumOfParamers.ONEP.name) //ONE PARAMETER
+    printResults(die3, 5, NumOfParamers.ONEP.name) //ONE PARAMETER
+    printResults(die4, 6, NumOfParamers.ONEP.name) //ONE PARAMETER
+    printResults(die5, 20, NumOfParamers.ONEP.name) //ONE PARAMETER
+   
+    printResults(die6, 20, "#FFA500")
+    printResults(die7, 3, "RED")
+    printResults(die8, 4, "GREEN")
+
+
 }
 
-fun printResults(die: Die, valuePassed: Int?) {
+fun printResults(die: Die, valuePassed: Int?, colorPassed: String?) {
      //RESULTS OF THE TEST CASES WITH constructor()
-    println("FOR: ${valuePassed} Default color of the dice: ${die.color}")
-    println("FOR: ${valuePassed} Default numberOfSide of the dice: ${die.numberOfSides}")
-    println("FOR: ${valuePassed} And the side currently showing is: ${die.sideUp}")
+    println()
+    if(colorPassed == NumOfParamers.ONEP.name || colorPassed == NumOfParamers.ZEROP.name) {
+        var currentParameter = NumOfParamers.values().find { it.name == colorPassed}
+        println("Number of parameters for this are: ${currentParameter?.meaning}")
+        println()
+    } 
+    println("FOR: ${valuePassed} && ${colorPassed} Default color of the dice: ${die.color}")
+    println("FOR: ${valuePassed} && ${colorPassed} Default numberOfSide of the dice: ${die.numberOfSides}")
+    println("FOR: ${valuePassed} && ${colorPassed} the side currently showing is: ${die.sideUp}")
+
+    println()
+    println()
+    println()
 }
--- Die.kt
+++ Die.kt
@@ -2,7 +2,7 @@ package game
 
 import kotlin.random.Random
 
-enum class Color(val otherName: String, val hexCode: String) {
+enum class Color(val other: String, val hexCode: String) {
     RED("d3", "#FF0000"),
     WHITE("d4", "#FFFFFF"),
     YELLOW("d6", "#FFFF00"),
@@ -33,16 +33,18 @@ class Die {
     var sideUp: Int? = null
 
     constructor() {
-        color = Color.values().find { it.otherName == "d6" }
+        color = Color.values().find { it.other == "d6" }
         numberOfSides = Sides.SIX
         roll()
     }
 
     constructor(numberOfSides: Int) {
         val validNumberOfSides = Sides.values().find { it.value == numberOfSides }
+       val colorName = validNumberOfSides?.let { "d${it.value}" }
+
         if(validNumberOfSides != null) {
             this.numberOfSides = validNumberOfSides
-            color = Color.values().find { it.otherName == "d${validNumberOfSides}" }
+            color = Color.values().find { it.other == colorName }
             roll()
         } else {
             println("We have dices with only dices with 3,4,6,20 sides only")
@@ -51,7 +53,7 @@ class Die {
 
     constructor(numOfSides:Int, selectedColor: String) {
         val validNumberOfSides = Sides.values().find { it.value == numOfSides }
-        val validColor = Color.values().find { it.otherName == selectedColor } ?: Color.values().find { it.hexCode == selectedColor }
+        val validColor = Color.values().find{ it.name == selectedColor }?: Color.values().find { it.other == selectedColor } ?: Color.values().find { it.hexCode == selectedColor }
         if(validColor != null && validNumberOfSides != null) {
             numberOfSides = validNumberOfSides
             color = validColor
@@ -60,7 +62,7 @@ class Die {
             println("Please enter valid values, values allowed for Colors are")
             val allColors = Color.values()
             for (color in allColors) {
-                println("Color: ${color.name}, Other Name: ${color.otherName}, Hex Code: ${color.hexCode}")
+                println("Color: ${color.name}, Other: ${color.other}, Hex Code: ${color.hexCode}")
             }
         }
     }
