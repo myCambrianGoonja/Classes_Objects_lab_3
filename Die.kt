181aa5f90888acfaef3b7b5fa35a5829ac20f3fd
myCambrianGoonja <A00277328@mycambrian.ca>
2023-06-16 11:33:52 -0400

    meaningful comments added to the methods

--- Die.kt
+++ Die.kt
@@ -60,36 +60,70 @@ class Die {
     private var color: Color? = null
     private var numberOfSides: Sides? = null
     var sideUp: Int? = null
-
+    
+    //Zero parameter constructor has color d6 and number of sides 6 
     constructor() {
+        /*
+        Color.values() loops through all the values of 
+        Color enum and .finds the value whos others value 
+        is 'd6' and sets that as the color value
+        */
         color = Color.values().find { it.other == "d6" }
         numberOfSides = Sides.SIX
         roll()
     }
-
+    
+    //Setting the value of numberOfSides and color is based on the numberOfSides
     constructor(numberOfSides: Int) {
+         /*
+            Sides.values() loops through all the values of 
+            Sides enum and .finds the value whos 'value' =  numberOfSides
+            and sets that as the validNumberOfSides
+        */
         val validNumberOfSides = Sides.values().find { it.value == numberOfSides }
+        /*
+            if the user enters value 21 or the number of value 
+            that is not set in the Sides enum class then the value 
+            stored in validNumberOfSides will be null thus 
+            validNumberOfSides? = meaning if it exists then 
+            colorName will be d{validNumberOfSides}
+         */
+       
         val colorName = validNumberOfSides?.let { "d${it.value}" }
 
+    //  This is a null varification of validNumberOfSides
         if(validNumberOfSides != null) {
             this.numberOfSides = validNumberOfSides
+            /*
+                Color values are looped and then the one that 
+                matches colorName is set to the color value
+            */
             color = Color.values().find { it.other == colorName }
             roll()
         } else {
+            //Guiding the user to enter the right values for the number of sides
             println("We have dices with only dices with 3,4,6,20 sides only")
             return
         }
     }
+    /*
+        Constructor helps assigning dynamic values to both color and number of sides
 
+        @param numOfSides = setting the numer of sides 
+        @param selectedColor = setting the color can be hexCode or 'd6'/'d4' etc or 'RED'
+     */
     constructor(numOfSides:Int, selectedColor: String) {
         val validNumberOfSides = Sides.values().find { it.value == numOfSides }
+        //Checking whether a name or other or a hexcode is passed which ever matches is choosen 
         val validColor = Color.values().find{ it.name == selectedColor }?: Color.values().find { it.other == selectedColor } ?: Color.values().find { it.hexCode == selectedColor }
+        //Null varification of validNumberOfSides and validColor and then assigning values
         if(validColor != null && validNumberOfSides != null) {
             numberOfSides = validNumberOfSides
             color = validColor
             roll()
         } else {
             println("Please enter valid values, values allowed for Colors are")
+            //Guiding the user to use the right values for colors 
             val allColors = Color.values()
             for (color in allColors) {
                 println("Color: ${color.name}, Other: ${color.other}, Hex Code: ${color.hexCode}")
@@ -107,6 +141,7 @@ class Die {
         return numberOfSides?.value
     }
 
+    //Method that sets sideUp into a random number that is <= numberOfSides
     fun roll() {
         sideUp = Random.nextInt(1, numberOfSides?.value?.plus(1)?: 1)
     }
